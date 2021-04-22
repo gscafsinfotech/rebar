a5857ecd54695a4806291143577bcafe64ffd8a4
unknown <vasanthkumar@cafsinfotech.in>
2021-04-22 18:34:54 +0530

    time sheet changes

--- application/views/alter.php
+++ application/views/alter.php
@@ -7,3 +7,17 @@ ALTER TABLE `cw_time_sheet_time_line` ADD `team` VARCHAR(255) NULL DEFAULT '0' A
 
 
 ALTER TABLE `cw_tonnage_approval` ADD `team` VARCHAR(255) NULL DEFAULT '0' AFTER `approval_status`;
+
+
+
+
+
+
+UPDATE `cw_form_setting` SET `field_type` = '3' WHERE `cw_form_setting`.`prime_form_id` = 1077;
+UPDATE `cw_form_setting` SET `field_type` = '3' WHERE `cw_form_setting`.`prime_form_id` = 1078;
+UPDATE `cw_form_setting` SET `field_type` = '3' WHERE `cw_form_setting`.`prime_form_id` = 1079;
+UPDATE `cw_form_setting` SET `field_type` = '3' WHERE `cw_form_setting`.`prime_form_id` = 1080;
+UPDATE `cw_form_setting` SET `field_type` = '3' WHERE `cw_form_setting`.`prime_form_id` = 1081;
+UPDATE `cw_form_setting` SET `field_type` = '3' WHERE `cw_form_setting`.`prime_form_id` = 1082;
+
+
